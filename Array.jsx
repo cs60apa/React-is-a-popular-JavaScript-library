c69f4aceb568488a6dfaa6fec464beb1e1275d67
marksikaundi <emmanuelmark117@gmail.com>
2023-03-30 17:16:28 +0200

    CORRECTED

--- Array.jsx
+++ Array.jsx
@@ -1,4 +1,5 @@
-{/*
+{
+  /*
 Pass an Array as Props
 The last challenge demonstrated how to pass information from a parent component to a child component as props or properties. This challenge looks at how arrays can be passed as props. To pass an array to a JSX element, it must be treated as JavaScript and wrapped in curly braces.
 
@@ -8,11 +9,16 @@ The last challenge demonstrated how to pass information from a parent component 
 The child component then has access to the array property colors. Array methods such as join() can be used when accessing the property. const ChildComponent = (props) => <p>{props.colors.join(', ')}</p> This will join all colors array items into a comma separated string and produce: <p>green, blue, red</p> Later, we will learn about other common methods to render arrays of data in React.
 
 There are List and ToDo components in the code editor. When rendering each List from the ToDo component, pass in a tasks property assigned to an array of to-do tasks, for example ["walk dog", "workout"]. Then access this tasks array in the List component, showing its value within the p element. Use join(", ") to display the props.tasksarray in the p element as a comma separated list. Today's list should have at least 2 tasks and tomorrow's should have at least 3 tasks.
-*/}
+*/
+}
 const List = (props) => {
-  { /* Change code below this line */ }
-  return <p>{}</p>
-  { /* Change code above this line */ }
+  {
+    /* Change code below this line */
+  }
+  return <p>{}</p>;
+  {
+    /* Change code above this line */
+  }
 };
 
 class ToDo extends React.Component {
@@ -24,21 +30,25 @@ class ToDo extends React.Component {
       <div>
         <h1>To Do Lists</h1>
         <h2>Today</h2>
-        { /* Change code below this line */ }
-        <List/>
+        {/* Change code below this line */}
+        <List />
         <h2>Tomorrow</h2>
-        <List/>
-        { /* Change code above this line */ }
+        <List />
+        {/* Change code above this line */}
       </div>
     );
   }
-};
+}
 
 // solution
 const List = (props) => {
-  { /* Change code below this line */ }
-  return <p>{}</p>
-  { /* Change code above this line */ }
+  {
+    /* Change code below this line */
+  }
+  return <p>{}</p>;
+  {
+    /* Change code above this line */
+  }
 };
 
 class ToDo extends React.Component {
@@ -50,17 +60,14 @@ class ToDo extends React.Component {
       <div>
         <h1>To Do Lists</h1>
         <h2>Today</h2>
-        { /* Change code below this line */ }
+        {/* Change code below this line */}
         {["walk", "eat", "barth"]}
-        <List/>
+        <List />
         <h2>Tomorrow</h2>
-        <List/>
-        { /* Change code above this line */ }
+        <List />
+        {/* Change code above this line */}
         {["Code", "Coffee", "Sleep"]}
       </div>
     );
   }
-};
-
-
-
+}
