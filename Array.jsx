a8949e7821f4351398b451255ecc2e6199e34e9a
marksikaundi <emmanuelmark117@gmail.com>
2023-03-30 17:10:20 +0200

    updates

--- Array.jsx
+++ Array.jsx
@@ -35,6 +35,32 @@ class ToDo extends React.Component {
 };
 
 // solution
+const List = (props) => {
+  { /* Change code below this line */ }
+  return <p>{}</p>
+  { /* Change code above this line */ }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        { /* Change code below this line */ }
+        {["walk", "eat", "barth"]}
+        <List/>
+        <h2>Tomorrow</h2>
+        <List/>
+        { /* Change code above this line */ }
+        {["Code", "Coffee", "Sleep"]}
+      </div>
+    );
+  }
+};
 
 
 
