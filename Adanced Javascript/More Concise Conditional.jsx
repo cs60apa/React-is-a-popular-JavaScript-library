c2193a80d5a7ae99a935fbcab41b7d053684d40b
marksikaundi <emmanuelmark117@gmail.com>
2023-04-02 23:47:33 +0200

    updates

--- Adanced Javascript/More Concise Conditional.jsx
+++ Adanced Javascript/More Concise Conditional.jsx
@@ -34,18 +34,25 @@ class MyComponent extends React.Component {
 }
 // Solution
 class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dinnerCooked: true,
-    };
-  }
-  render() {
-    return (
-      <div>
-        {this.state.dinnerCooked && <h1>Dinner is Cooked!</h1>}//h1 tag contents
-        will be displayed
-      </div>
-    );
-  }
-}
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+   this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+      this.setState(state => ({
+        display: !state.display
+      }));
+    }
+    render() {
+      // change code below this line
+      return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+           {this.state.display && <h1>Displayed!</h1>}
+         </div>
+      );
+    }
+  };
